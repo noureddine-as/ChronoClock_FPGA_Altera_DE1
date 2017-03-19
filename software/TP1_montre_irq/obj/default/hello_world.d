fc18eefa859fda9993d7e718e70ebd92a6b572a5
noureddine-as <noureddine.asni@gmail.com>
2017-03-19 02:23:09 +0000

    Interruptions activated on SWs and KEYs .. commands work well .. U still need to fix the scew caused by usleep() and use Timer

--- software/TP1_montre_irq/obj/default/hello_world.d
+++ software/TP1_montre_irq/obj/default/hello_world.d
@@ -1,1 +1,44 @@
-obj/default/hello_world.o: hello_world.c
+obj/default/hello_world.o: hello_world.c ../TP1_montre_irq_bsp/system.h \
+  ../TP1_montre_irq_bsp/linker.h \
+  ../TP1_montre_irq_bsp//drivers/inc/altera_up_avalon_parallel_port_regs.h \
+  ../TP1_montre_irq_bsp//HAL/inc/io.h \
+  ../TP1_montre_irq_bsp//HAL/inc/alt_types.h \
+  ../TP1_montre_irq_bsp//HAL/inc/sys/alt_irq.h \
+  ../TP1_montre_irq_bsp//HAL/inc/nios2.h \
+  ../TP1_montre_irq_bsp//HAL/inc/alt_types.h \
+  ../TP1_montre_irq_bsp/system.h \
+  ../TP1_montre_irq_bsp//HAL/inc/priv/alt_legacy_irq.h \
+  ../TP1_montre_irq_bsp/system.h ../TP1_montre_irq_bsp//HAL/inc/nios2.h \
+  ../TP1_montre_irq_bsp//HAL/inc/alt_types.h \
+  ../TP1_montre_irq_bsp//HAL/inc/sys/alt_irq.h \
+  ../TP1_montre_irq_bsp//HAL/inc/alt_types.h
+
+../TP1_montre_irq_bsp/system.h:
+
+../TP1_montre_irq_bsp/linker.h:
+
+../TP1_montre_irq_bsp//drivers/inc/altera_up_avalon_parallel_port_regs.h:
+
+../TP1_montre_irq_bsp//HAL/inc/io.h:
+
+../TP1_montre_irq_bsp//HAL/inc/alt_types.h:
+
+../TP1_montre_irq_bsp//HAL/inc/sys/alt_irq.h:
+
+../TP1_montre_irq_bsp//HAL/inc/nios2.h:
+
+../TP1_montre_irq_bsp//HAL/inc/alt_types.h:
+
+../TP1_montre_irq_bsp/system.h:
+
+../TP1_montre_irq_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../TP1_montre_irq_bsp/system.h:
+
+../TP1_montre_irq_bsp//HAL/inc/nios2.h:
+
+../TP1_montre_irq_bsp//HAL/inc/alt_types.h:
+
+../TP1_montre_irq_bsp//HAL/inc/sys/alt_irq.h:
+
+../TP1_montre_irq_bsp//HAL/inc/alt_types.h:
